7a28d1ffc5dff5ef653656ccc733d513a1f591bd
Ibelema <ibelemaiwoh02@gmail.com>
2023-02-24 15:04:23 +0100

    a new css added

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ class TodoList(models.Model):
     title = models.CharField(max_length=140)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
-
+    
     class Meta:
         ordering = ('-date_created',)
         verbose_name = ('TodoList')
